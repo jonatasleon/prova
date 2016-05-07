7f434a3fcda21a69e06cbebc8e0f521f6416f9fd
Jonatas Leon <jonatas.leon@gmail.com>
2016-05-07 00:05:56 -0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,25 @@
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
+var runSequence = require('run-sequence');
+
+gulp.task('build', function() {
+    runSequence(
+        'copy-files'
+    )
+});
+
+gulp.task('copy-files', function() {
+    var files = [
+        './index.html',
+        './README.md',
+        './assets/**/*',
+        './.travis.yml'
+    ]
+    return gulp.src(files, {
+        base: './'
+    }).pipe(gulp.dest('build/'))
+})
 
 gulp.task('minify-img', function() {
     return gulp.src('assets/img/src/**/*')
